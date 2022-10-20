bd1ffaa64be6470ec9902642a6f22f10f56e3038
En1o <andrey.p0209@gmail.com>
2022-10-20 21:54:42 +0200

    Exercises 2 and 3
    complited exercises 2 and 3 + started some html

--- practice.js
+++ practice.js
@@ -3,3 +3,21 @@
 let sideSquare = 2;
 let perimeter = sideSquare * 4;
 console.log("Периметр квадрата раваен: " + sideSquare); 
+
+/* Дано целое число А. Проверить истинность высказывания: "Число А является положительным." */
+
+let num = 33;
+if (num > 0){
+    console.log("Число А положительное!");
+} else {
+    console.log("Число А не является положительным.");
+}
+
+/* Даны два числа А и В. Проверить истинность высказывания: "A > 2 и B <= 3" */
+
+let numA = 7, numB = 3;
+if (numA > 2 && numB <= 3){
+    console.log(true);
+} else {
+    console.log(false)
+}
